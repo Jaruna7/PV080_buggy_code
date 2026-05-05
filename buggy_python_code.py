e60bd900c030f823179a3a78c4d3a79f81e96f5d
Jaruna7 <mensikovlenka666@gmail.com>
2026-05-05 14:54:45 +0200

    aaaaaaaaaaaaaaaaaaaa

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,20 +1,18 @@
-import sys
-import os
 import yaml
 import flask
 
-app = flask.Flask(__name__)
+APP = flask.Flask(__name__)
 
 
-@app.route("/")
+@APP.route("/")
 def index():
     """sdvsdv"""
     version = flask.request.args.get("urllib_version")
     url = flask.request.args.get("url")
     return fetch_website(version, url)
-        
+
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
-class Person(object):
+class Person():
     """sdvsdv"""
     def __init__(self, name):
         self.name = name
@@ -32,7 +30,6 @@ def fetch_website(urllib_version, url):
     # Fetch and print the requested URL
     try:
         http = urllib_version.PoolManager()
-        r = http.request('GET', url)
     except:
         print('Exception')
 
@@ -42,12 +39,14 @@ def load_yaml(filename):
     stream = open(filename)
     deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
-    
+
+
 def authenticate(password):
     """dfvdfvdf"""
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
+
 
 if __name__ == '__main__':
     print("Vulnerabilities:")
@@ -55,17 +54,16 @@ if __name__ == '__main__':
     print("2. Code injection vulnerability:")
     print("3. Yaml deserialization vulnerability:")
     print("4. Use of assert statements vulnerability:")
-    choice  = input("Select vulnerability: ")
-    if choice == "1":
+    CHOICE = input("Select vulnerability: ")
+    if CHOICE == "1":
         NEW_PERSON = Person("Vickie")
         print_nametag(input("Please format your nametag: "), NEW_PERSON)
-    elif choice == "2":
-        urlib_version = input("Choose version of urllib: ")
-        fetch_website(urlib_version, url="https://www.google.com")
-    elif choice == "3":
+    elif CHOICE == "2":
+        URLIB_VERSION = input("Choose version of urllib: ")
+        fetch_website(URLIB_VERSION, url="https://www.google.com")
+    elif CHOICE == "3":
         load_yaml(input("File name: "))
         print("Executed -ls on current folder")
-    elif choice == "4":
+    elif CHOICE == "4":
         password = input("Enter master password: ")
         authenticate(password)
-
